dea9987aa604211c7e7c1a51f50d17fab685e377
dilermando <di_ler@hotmail.com>
2019-02-08 21:06:27 -0200

    MVP + no reload

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.java
@@ -1,96 +1,54 @@
 package com.arctouch.codechallenge.home;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import com.arctouch.codechallenge.R;
-import com.arctouch.codechallenge.api.TmdbApi;
-import com.arctouch.codechallenge.base.BaseActivity;
-import com.arctouch.codechallenge.data.Cache;
-import com.arctouch.codechallenge.model.Genre;
-import com.arctouch.codechallenge.model.Movie;
 
-import java.util.ArrayList;
-import java.util.List;
+public class HomeActivity extends AppCompatActivity implements HomeView {
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-
-public class HomeActivity extends BaseActivity {
-
+    private HomePresenter mPresenter;
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
-    private boolean isFetchingMovies;
-    private Integer currentPage = 1;
-    private List<Movie> mMovieList = new ArrayList<>();
-    private HomeAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
-        this.recyclerView = findViewById(R.id.recyclerView);
-        this.progressBar = findViewById(R.id.progressBar);
+        init();
 
-        getMovies(1);
+        mPresenter = new HomePresenter(this, this);
+        mPresenter.getGenres();
+        mPresenter.getMovies(1);
     }
 
-    private void getMovies(final Integer page) {
-        isFetchingMovies = true;
 
-        api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, page)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(response -> {
-                    mMovieList = response.results;
 
-                    for (Movie movie : mMovieList) {
-                        movie.genres = new ArrayList<>();
-                        for (Genre genre : Cache.getGenres()) {
-                            if (movie.genreIds.contains(genre.id)) {
-                                movie.genres.add(genre);
-                            }
-                        }
-                    }
+    @Override
+    public void init() {
+        this.recyclerView = findViewById(R.id.recyclerView);
+        this.progressBar = findViewById(R.id.progressBar);
+    }
 
-                    final LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
 
-                    if (adapter == null) {
-                        adapter = new HomeAdapter(this, mMovieList);
-                        recyclerView.setAdapter(adapter);
-                    } else {
-                        adapter.appendMovies(mMovieList);
-                    }
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
 
-                    recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-                        @Override
-                        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                            super.onScrolled(recyclerView, dx, dy);
-
-                            if (dy > 0 && mMovieList.size() > 0) {
-                                int totalItemCount = manager.getItemCount();
-                                int visibleItemCount = manager.getChildCount();
-                                int firstVisibleItem = manager.findFirstVisibleItemPosition();
-
-                                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
-                                    if (!isFetchingMovies) {
-                                        progressBar.setVisibility(View.VISIBLE);
-                                        getMovies(currentPage + 1);
-                                    }
-                                }
-                            }
-                        }
-                    });
-
-                    currentPage = page;
-                    isFetchingMovies = false;
-                    progressBar.setVisibility(View.GONE);
-                });
+    @Override
+    public RecyclerView getRecyclerView() {
+        return recyclerView;
     }
 
 }
--- app/src/main/java/com/arctouch/codechallenge/home/HomePresenter.java
+++ app/src/main/java/com/arctouch/codechallenge/home/HomePresenter.java
@@ -1,4 +1,95 @@
 package com.arctouch.codechallenge.home;
 
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.arctouch.codechallenge.api.TmdbApi;
+import com.arctouch.codechallenge.api.TmdbRetrofit;
+import com.arctouch.codechallenge.data.Cache;
+import com.arctouch.codechallenge.model.Genre;
+import com.arctouch.codechallenge.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
 public class HomePresenter {
+
+    private HomeView mView;
+    private Context mContext;
+    private HomeAdapter adapter;
+    private List<Movie> mMovieList = new ArrayList<>();
+    private boolean isFetchingMovies;
+    private int currentPage = 1;
+
+    public HomePresenter(HomeView view, Context context) {
+        mView = view;
+        mContext = context;
+    }
+
+    public void getGenres() {
+        TmdbRetrofit.api().genres(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> {
+                    Cache.setGenres(response.genres);
+                });
+    }
+
+    public void getMovies(final int page) {
+        isFetchingMovies = true;
+
+        TmdbRetrofit.api().upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, page)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> {
+                    mMovieList = response.results;
+
+                    for (Movie movie : mMovieList) {
+                        movie.genres = new ArrayList<>();
+                        for (Genre genre : Cache.getGenres()) {
+                            if (movie.genreIds.contains(genre.id)) {
+                                movie.genres.add(genre);
+                            }
+                        }
+                    }
+
+                    final LinearLayoutManager manager = (LinearLayoutManager) mView.getRecyclerView().getLayoutManager();
+
+                    if (adapter == null) {
+                        adapter = new HomeAdapter(mContext, mMovieList);
+                        mView.getRecyclerView().setAdapter(adapter);
+                    } else {
+                        adapter.appendMovies(mMovieList);
+                    }
+
+                    mView.getRecyclerView().addOnScrollListener(new RecyclerView.OnScrollListener() {
+                        @Override
+                        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                            super.onScrolled(recyclerView, dx, dy);
+
+                            if (dy > 0 && mMovieList.size() > 0) {
+                                int totalItemCount = manager.getItemCount();
+                                int visibleItemCount = manager.getChildCount();
+                                int firstVisibleItem = manager.findFirstVisibleItemPosition();
+
+                                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
+                                    if (!isFetchingMovies) {
+                                        mView.showProgress();
+                                        getMovies(currentPage + 1);
+                                    }
+                                }
+                            }
+                        }
+                    });
+
+                    currentPage = page;
+                    isFetchingMovies = false;
+                    mView.hideProgress();
+                });
+    }
+
 }
